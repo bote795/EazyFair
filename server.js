34198f721e09807a876ce904fb235ee172df641d
bote795 <nbotell2@gmail.com>
2015-09-06 00:23:42 +0000

    adding server side script

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+    app = express();
+// Retrieve
+  Db = require('mongodb').Db,
+  Server = require('mongodb').Server,
+  Connection = require('mongodb').Connection, 
+  MongoClient = require('mongodb').MongoClient;
+  var db= new Db("test", new Server("localhost","27017"));
+// Connect to the db
+/*MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+  if(!err) {
+    dB = db;
+    console.log("We are connected");
+  }
+});
+*/
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+app.get('/add', function(req, res){
+    db.open(function(err, p_db) {
+      if(!err){
+      var table = db.collection("test");
+      table.insert({"name":"food"});  
+      p_db.close();
+      console.log("insertion");
+    }
+    });
+    res.send('sucess');
+});
+
+
+
+app.listen(3000);
